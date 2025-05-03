58489a0bf9c6a9e08916a3fbd2846a1f6f8a0250
B1n_FreeToRoAm <z2662728664@gmail.com>
2025-05-03 12:31:48 +0800

    feat: Ollama DeepSeek 流式应答接口实现

---
+++ dev-tech-api/src/main/java/com/b1n/dev/tech/api/IAiService.java
@@ -0,0 +1,14 @@
+package com.b1n.dev.tech.api;
+
+import org.springframework.ai.chat.ChatResponse;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author zhangbin
+ */
+public interface IAiService {
+
+    ChatResponse generate(String model, String message);
+
+    Flux<ChatResponse> generateStream(String model, String message);
+}
