494d0b7ac3e45fc67d63a9bb776335c31e8dcf54
B1n_FreeToRoAm <z2662728664@gmail.com>
2025-05-03 17:58:32 +0800

    feat: Ollama DeepSeek 流式应答前端实现

--- dev-tech-trigger/src/main/java/com/b1n/dev/tech/trigger/http/OllamaController.java
+++ dev-tech-trigger/src/main/java/com/b1n/dev/tech/trigger/http/OllamaController.java
@@ -28,7 +28,7 @@ public class OllamaController implements IAiService {
      */
     @RequestMapping(value = "generate", method = RequestMethod.GET)
     @Override
-    public ChatResponse generate(@RequestParam String model, @RequestParam String message) {
+    public ChatResponse generate(@RequestParam(name = "model") String model, @RequestParam(name = "message") String message) {
         return chatClient.call(new Prompt(message, OllamaOptions.create().withModel(model)));
     }
 
@@ -40,7 +40,7 @@ public class OllamaController implements IAiService {
      */
     @RequestMapping(value = "generate_stream", method = RequestMethod.GET)
     @Override
-    public Flux<ChatResponse> generateStream(@RequestParam String model, @RequestParam String message) {
+    public Flux<ChatResponse> generateStream(@RequestParam(name = "model") String model, @RequestParam(name = "message") String message) {
         return chatClient.stream(new Prompt(message, OllamaOptions.create().withModel(model)));
     }
 }
