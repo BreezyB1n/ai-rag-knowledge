6b8909852c046f500ef2f2af11632057d7fa4842
B1n_FreeToRoAm <z2662728664@gmail.com>
2025-05-05 10:26:50 +0800

    feat: rag git repo 解析上传功能实现

--- dev-tech-api/src/main/java/com/b1n/dev/tech/api/IRAGService.java
+++ dev-tech-api/src/main/java/com/b1n/dev/tech/api/IRAGService.java
@@ -1,16 +1,29 @@
 package com.b1n.dev.tech.api;
 
 import com.b1n.dev.tech.api.response.Response;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 /**
+ * RAG服务接口
  * @author zhangbin
  */
 public interface IRAGService {
-
+    /**
+     * 查询RAG标签列表
+     * @return 标签列表
+     */
     Response<List<String>> queryRagTagList();
 
+    /**
+     * 上传文件到知识库
+     * @param ragTag 知识库标签
+     * @param files 要上传的文件列表
+     * @return 上传结果
+     */
     Response<String> upload(String ragTag, List<MultipartFile> files);
+
+    Response<String> analyzeGitRepository(String repoUrl, String userName, String token) throws Exception;
 }
